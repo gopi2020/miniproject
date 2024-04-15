fe9d99e1d0df8a9e4be45e21e8779a2ee280ce86
RKAPP5 <58256501+gopi2020@users.noreply.github.com>
2024-04-15 13:26:39 +0530

    Update loop.dart
    new code

--- loop.dart
+++ loop.dart
@@ -1,9 +1,43 @@
-void main(){
-  int i=1, j=2;
+// void main() {
+//   int i, num = 3;
+// while (i < 10) {
+//   print(i);
+//   i++;
+// }
 
+// ignore_for_file: unnecessary_brace_in_string_interps
 
-  while(i<=10){
-    print('$j * $i = ${j*i}');
-    i++;
+// do {
+//   print(i);
+//   i++;
+// } while (i < 5);
+// for (i = 1; i <= 20; i++) {
+//   if (i.isOdd) {
+//     continue;
+//   }
+//   print('$num * $i= ${num * i}');
+// }
+// }
+// while (i <= 10) {
+//   int j = 1;
+//   while (j <= 10) {
+//     print('$i*$j=${i * j}');
+//     j++;
+//   }
+//   i++;
+// }
+import 'dart:io';
+
+void main() {
+  while (true) {
+    try {
+      print("enter your birth year");
+      var birth = int.parse(stdin.readLineSync()!);
+      var age = DateTime.now().year - birth.abs();
+      print("your age is ${age} years");
+      break;
+    } catch (e) {
+      print("your value invaild");
+    }
   }
 }
