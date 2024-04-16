d425a63347696853bb82cbb77c074b066377efab
gopirk <gopigopu00@gmail.com>
2024-04-16 11:59:52 +0530

    output

---
+++ output.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  print("hi friends");
+  stdout.write("welcome to apps");
+  print('welcome');
+  print("welocme");
+  print('''
+This page describes solutions to problems 
+that might occur when you're trying to 
+use DartPad, whether at dartpad.dev 
+''');
+  print('welcome to app\'s');
+  print("welcome to app's");
+  print('4+5= ${4 + 5}');
+}
