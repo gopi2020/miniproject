e5e54953836a1ec2b369da8b7834fbb95c0cc7fb
gopirk <gopigopu00@gmail.com>
2024-04-16 11:14:05 +0530

    finally set

--- sets.dart
+++ sets.dart
@@ -10,4 +10,19 @@ void main() {
     print(list);
   });
   print(setlist.toList());
+
+  var a = {1, 2, 3, 4};
+  var b = {3, 4, 5, 6};
+  print("a=$a");
+  print("b=$b");
+  print(a.union(b));
+  print(a.intersection(b));
+  print(a.difference(b));
+  print(b.difference(a));
+  print(a.length);
+  print(a.indexed);
+  print(a.firstOrNull);
+  print(a.runtimeType);
+  a.addAll({8, 9});
+  print("a=$a");
 }
