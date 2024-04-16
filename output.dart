e1e97a13df807137989b2c9bc30a41e871b18b03
gopirk <gopigopu00@gmail.com>
2024-04-16 12:03:05 +0530

    output finally

--- output.dart
+++ output.dart
@@ -3,14 +3,16 @@ import 'dart:io';
 void main() {
   print("hi friends");
   stdout.write("welcome to apps");
-  print('welcome');
-  print("welocme");
+  print('Hello world'); //single Quote
+  print("Hello world"); //double Quote
   print('''
 This page describes solutions to problems 
 that might occur when you're trying to 
 use DartPad, whether at dartpad.dev 
 ''');
-  print('welcome to app\'s');
   print("welcome to app's");
+  print('welcome to app\'s');
+  print("price:45\$");
+  print(4 + 6);
   print('4+5= ${4 + 5}');
 }
